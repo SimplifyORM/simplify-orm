a6c20f4b878cd20c5a02dc0ba489bc11d8be5c0e
LamaDev <hlama@live.fr>
2017-10-11 23:25:11 +0100

    Stable core generator pojos

--- main.js
+++ main.js
@@ -4,16 +4,32 @@
  * MIT Licensed
  */
 
-
 'use strict';
-
 
 /**
  * Module dependencies.
  * @private
  */
 
-let init =require('./lib/initializer');
+let init = require('./lib/initializer');
+let queryBuilder = require('./lib/queryBuider');
+
+let dbInfo;
+
+/**
+ * add comment 
+ * @param  {JSON} `dbconfig`
+ * @param  {JSON} `mapping`
+ * @private
+ */
+
+let initialize = (dbconfig) => {
+
+    dbInfo = dbconfig
+        //queryBuilder.init(dbInfo,mapping)
+    createModels();
+};
+
 
 /**
  * add comment 
@@ -21,10 +37,32 @@ let init =require('./lib/initializer');
  * @private
  */
 
-let initialize = (dbconfig) => { 
+let createModels = () => {
 
-        init.init(dbconfig)
+    init.init(dbInfo)
 
+};
+
+/**
+ * add comment 
+ * @param  {Object} `obj`
+ * @private
+ */
+
+let saveObject = (obj) => {
+
+    queryBuilder.save(obj);
+};
+
+/**
+ * add comment 
+ * @param  {Object} `obj`
+ * @private
+ */
+
+let updateObject = (obj) => {
+
+    queryBuilder.update(obj);
 };
 
 
@@ -33,6 +71,11 @@ let initialize = (dbconfig) => {
  * @public
  */
 
-exports.initialize = initialize;
+module.exports = {
 
+    initialize: initialize,
+    createModels: createModels,
+    saveObject: saveObject,
+    updateObject: updateObject
 
+}
