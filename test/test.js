bd079ba3b768f1d7cb56e8959aab303a108b185e
LamaDev <hlama@live.fr>
2017-10-11 23:22:01 +0100

    Stable core generator pojos

--- test/test.js
+++ test/test.js
@@ -1,5 +1,31 @@
 'use strict';
-let main = require('../main');
+
+let context = require('../main');
 let myConfig = require('./db.json');
 
-main.initialize(myConfig);
+context.initialize(myConfig);
+
+//var content = JSON.stringify(mappingFile);
+
+//var data = JSON.parse(content);
+
+//console.log(data[1].properties);
+
+
+
+
+
+//let profile = new obj.profile("","Msat","Msattoure","Toores");
+
+//context.createModels();
+
+//let emp = new obj.('12','Toure','msat@gmail.com','0811214456');
+
+//let emp2 = new obj.employee();
+
+//context.saveObject(profile);
+
+// emp2.email = "you@gmail.com";
+// emp2.telephone = "O96789768";
+
+// let requete = context.updateObject(emp2);
