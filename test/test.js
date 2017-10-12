0fb3109626da22b5d98056a2cbf9e62c94e67fb9
Hornel Lama <hlama@live.fr>
2017-10-12 20:03:17 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 'use strict';
 
 let context = require('../main');
@@ -6,35 +6,3 @@ let myConfig = require('./db.json');
 
 context.initialize(myConfig);
 
-//var content = JSON.stringify(mappingFile);
-
-//var data = JSON.parse(content);
-
-//console.log(data[1].properties);
-
-
-
-
-
-//let profile = new obj.profile("","Msat","Msattoure","Toores");
-
-//context.createModels();
-
-//let emp = new obj.('12','Toure','msat@gmail.com','0811214456');
-
-//let emp2 = new obj.employee();
-
-//context.saveObject(profile);
-
-// emp2.email = "you@gmail.com";
-// emp2.telephone = "O96789768";
-
-// let requete = context.updateObject(emp2);
-=======
-'use strict';
-
-let main = require('../main');
-let myConfig = require('./db.json');
-
-main.initialize(myConfig);
->>>>>>> b7bd25b224f1d4180203c059d2b766ab79f3a363
